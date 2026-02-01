60dcbe1f5c43491731a737f49fff4032118cf6bc
BSquad <121628964+BSquad@users.noreply.github.com>
2026-02-01 16:11:26 +0100

    moved testApiCall

--- js/api/api.js
+++
@@ -1,9 +0,0 @@
-const path = location.hostname === "localhost"
-  ? "http://localhost:3000"
-  : "https://api.deinedomain.de";
-
-async function TestCall() {
-  const response = await fetch(path + "/test");
-  const data = await response.json();
-  console.log(data);
-}
--- js/logic/fetch.js
+++ js/logic/fetch.js
@@ -1,7 +1,17 @@
-async function fetchTSV(url) {
+const path = location.hostname === "localhost"
+  ? "http://localhost:3000"
+  : "https://api.deinedomain.de";
+
+  async function fetchTSV(url) {
   const res = await fetch(url);
   const tsvText = await res.text();
   const lines = tsvText.trim().split("\n");
   const headers = lines.shift().split("\t").map(h => h.trim());
   return lines.map(line => Object.fromEntries(headers.map((h, i) => [h, line.split("\t")[i]?.trim()])));
+}
+
+async function TestServerCall() {
+  const response = await fetch(path + "/test");
+  const data = await response.json();
+  console.log(data);
 }

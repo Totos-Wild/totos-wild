c5949faf4bfa2c3dfac81e0fc812bc216cf4c866
BSquad <121628964+BSquad@users.noreply.github.com>
2026-08-14 15:58:01 +0200

    split url

--- js/logic/fetch.js
+++ js/logic/fetch.js
@@ -1,6 +1,7 @@
 async function loadData(typ) {
-  const piUrl = "https://www-totos-wild-und-honig-de.k2x1vmwia2xhdx8w.myfritz.net/totos-wild-server/produkte.php";
-  const response = await fetch(`${piUrl}?typ=${encodeURIComponent(typ)}`);
+  const piUrl = "https://www-totos-wild-und-honig-de.k2x1vmwia2xhdx8w.myfritz.net";
+  const produktePfad = "/totos-wild-server/produkte.php";
+  const response = await fetch(`${piUrl}${produktePfad}?typ=${encodeURIComponent(typ)}`);
   const daten = await response.json();
   return daten;
 }

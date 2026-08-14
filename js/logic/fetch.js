8dc52de4dfb30f9c5756ab7b833543308c4a35d9
BSquad <121628964+BSquad@users.noreply.github.com>
2026-08-14 13:08:32 +0200

    kommunikation

--- js/logic/fetch.js
+++ js/logic/fetch.js
@@ -1,12 +1,6 @@
 async function loadData(typ) {
-  const piUrl = "https://www-totos-wild-und-honig-de.k2x1vmwia2xhdx8w.myfritz.net";
-  const response = await fetch(`${piUrl}?typ=${typ}`);
+  const piUrl = "https://www-totos-wild-und-honig-de.k2x1vmwia2xhdx8w.myfritz.net/totos-wild-server/produkte.php";
+  const response = await fetch(`${piUrl}?typ=${encodeURIComponent(typ)}`);
   const daten = await response.json();
   return daten;
-}
-
-async function TestServerCall() {
-  const response = await fetch(piUrl + "/test");
-  const data = await response.json();
-  console.log(data);
 }
